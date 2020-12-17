96a5e02708b6d37e9d44f801029a196fb4e0e5d8
djanzou120 <djanzou120@yahoo.fr>
2020-12-17 13:40:26 +0100

    with loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
 $(document).ready(() => {
-    gsap.from(['.img-profil',".hello-world", ".name-title", ".job-title", ".presentation", ".fill-contact"], { visibility:0 ,opacity: 0, duration: 2, y: 50});
-    gsap.from([".sub-liner"], {visibility: 0, opacity: 0, duration: 3, y: 90})
+    gsap.from([".loader-commas"], {visibility: 1, opacity: 1, duration: 2, repeat: -1, ease: "strong.inOut", yoyo: true});
+
 });
